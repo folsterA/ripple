43011bd2c8acd2c8f67164d9ff596c87d1cc7dbe
folsterA <folsteraustin@gmail.com>
2026-04-12 21:28:07 -0700

    wip: testing stuff with tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,19 @@
 
   let { children } = $props();
 
-  import "../app.css"
+  import '../app.css';
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="flex h-screen">
+  <aside class="flex flex-col w-32 bg-gray-500 space-y-2 p-2">
+    <div>Sidebar</div>
+    <div>Test</div>
+  </aside>
+  <main class="flex-1 bg-gray-700 p-4 overflow-auto">
+    {@render children()}
+  </main>
+</div>
