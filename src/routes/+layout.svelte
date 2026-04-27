0b5f59a8b07f2c59fd0302c06aed4b9a89480ff4
folsterA <folsteraustin@gmail.com>
2026-04-26 19:45:23 -0700

    feat: add tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
   import favicon from '$lib/assets/favicon.svg';
+  import { resolve } from '$app/paths';
+  import ExternalLinkHandler from '$lib/ExternalLinkHandler.svelte';
 
   let { children } = $props();
+
+  import '../app.css';
 </script>
+
+<ExternalLinkHandler />
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="flex h-screen">
+  <aside class="flex flex-col w-32 bg-gray-500 space-y-2 p-2">
+    <nav>
+      <ul>
+        <li><a href={resolve('/')}>test</a></li>
+        <li><a href="https://www.google.com" rel="external">test4141</a></li>
+        <li>test3</li>
+      </ul>
+    </nav>
+  </aside>
+  <main class="flex-1 bg-gray-700 p-4 overflow-auto">
+    {@render children()}
+  </main>
+</div>
