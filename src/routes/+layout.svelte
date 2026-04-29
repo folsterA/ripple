e42c3796eb5d7ced335499326c3e1e180adefa70
folsterA <folsteraustin@gmail.com>
2026-04-28 21:35:26 -0700

    wip: messing around with message formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,17 +14,18 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="flex h-screen">
+<div class="flex h-screen pr-1 bg-gray-700">
   <aside class="flex flex-col w-32 bg-gray-500 space-y-2 p-2">
     <nav>
       <ul>
-        <li><a href={resolve('/')}>test</a></li>
-        <li><a href="https://www.google.com" rel="external">test4141</a></li>
+        <li><a href={resolve('/')}>home</a></li>
+        <li><a href={resolve('/text')}>texts</a></li>
+        <li><a href="https://www.google.com" rel="external">external</a></li>
         <li>test3</li>
       </ul>
     </nav>
   </aside>
-  <main class="flex-1 bg-gray-700 p-4 overflow-auto">
+  <main class="flex-1 pl-3 bg-gray-700 overflow-auto custom-scrollbar">
     {@render children()}
   </main>
 </div>
