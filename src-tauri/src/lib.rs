056b8a327a86df823285b69b5b857ea661a9443c
folsterA <folsteraustin@gmail.com>
2026-05-05 21:54:31 -0700

    wip: create test data script

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,13 +3,26 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
+            let app_handle = app.handle();
             if cfg!(debug_assertions) {
-                app.handle().plugin(
+                app_handle.plugin(
                     tauri_plugin_log::Builder::default()
                         .level(log::LevelFilter::Info)
                         .build(),
                 )?;
+
+                let cwd = std::env::current_dir().expect("failed to get cwd");
+                let script = cwd
+                    .parent()
+                    .expect("no parent directory")
+                    .join("scripts/generate_test_data.py");
+
+                std::process::Command::new("python")
+                    .arg(script)
+                    .status()
+                    .expect("failed to generate test data");
             }
+
             Ok(())
         })
         .run(tauri::generate_context!())
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(debug_assertions)]
+    {}
+
     app_lib::run();
 }
